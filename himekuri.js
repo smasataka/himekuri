f7a7e514de098a3a1cf4a361c0213d9745e60f71
smasataka <61925565+smasataka@users.noreply.github.com>
2020-03-08 17:45:10 +0900

    Update himekuri.js

--- himekuri.js
+++ himekuri.js
@@ -30,7 +30,7 @@ function getDate() {
    var hour = fmtNumber( now.getHours() );
    var min  = fmtNumber( now.getMinutes() );
    var sec  = fmtNumber( now.getSeconds() );
-	document.getElementById("date").innerHTML = year + "年" + month + "月" + date + "日";
-	document.getElementById("time").innerHTML = week + "曜日" + hour + "時" + min + "分" + sec + "秒";
+	document.getElementById("date").innerHTML = year + "年" + month + "月" + date + "日" + "　" + week + "曜日";
+	document.getElementById("time").innerHTML = hour + "時" + min + "分" + sec + "秒";
 }
 setInterval('getDate()',1000);
