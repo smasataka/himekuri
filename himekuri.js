8b7b0bafc1cf733d5621c80d5be74686a2411fb0
smasataka <61925565+smasataka@users.noreply.github.com>
2020-03-08 12:56:13 +0900

    Update himekuri.js

--- himekuri.js
+++ himekuri.js
@@ -4,15 +4,15 @@ function fmtNumber(arg) {
    return arg;
 }
 function showDate() {
-   var now = new Date();
+   //var now = new Date();
    //var year = now.getFullYear();
    //var month = now.getMonth() + 1;
    //var date = now.getDate();
-   var nowHour = fmtNumber( now.getHours() );
-   var nowMin  = fmtNumber( now.getMinutes() );
-   var nowSec  = fMtNumber( now.getSeconds() );
+   //var nowHour = fmtNumber( now.getHours() );
+   //var nowMin  = fmtNumber( now.getMinutes() );
+   //var nowSec  = fMtNumber( now.getSeconds() );
    //var msg = year + "年" + month + "月" + date + "日";
-   var msg = nowHour;
+   var msg = "test";
    document.getElementById("RealtimeClockArea2").innerHTML = msg;
 }
 setInterval('showDate()',1000);
