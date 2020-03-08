02e1606b7bc84e2eb71ff128b707891c103741e8
smasataka <61925565+smasataka@users.noreply.github.com>
2020-03-08 17:31:01 +0900

    Add files via upload

--- himekuri.js
+++ himekuri.js
@@ -1,18 +1,36 @@
-//function fmtNumber(arg) {
-   // 桁数が1桁だったら先頭に0を加えて2桁に調整する
-   //if( arg < 10 ) { ret = "0" + arg; }
-   //return arg;
-//}
-function showDate() {
-   //var now = new Date();
-   //var year = now.getFullYear();
-   //var month = now.getMonth() + 1;
-   //var date = now.getDate();
-   //var nowHour = fmtNumber( now.getHours() );
-   //var nowMin  = fmtNumber( now.getMinutes() );
-   var nowSec  = fMtNumber( now.getSeconds() );
-   //var msg = year + "年" + month + "月" + date + "日";
-   var msg = nowSec;
-   document.getElementById("RealtimeClockArea2").innerHTML = msg;
+// 桁数が1桁だったら先頭に0を加えて2桁に調整する
+function fmtNumber(arg) {
+   if( arg < 10 ) { return "0" + arg; }
+   else { return arg; }
 }
-setInterval('showDate()',1000);
+
+// 日本語の曜日に返還
+function fmtWeek(arg) {
+	switch (arg)
+	{
+    case 0: return "日"; break;
+    case 1: return "月"; break;
+    case 2: return "火"; break;
+    case 3: return "水"; break;
+    case 4: return "木"; break;
+    case 5: return "金"; break;
+    case 6: return "ド"; break;
+    case 7: return "日"; break;
+    default: return "エラー"; break;
+	}
+}
+
+// メイン
+function getDate() {
+   var now = new Date();
+   var year = now.getFullYear();
+   var month = now.getMonth() + 1;
+   var date = now.getDate();
+   var week = fmtWeek( now.getDay() );
+   var hour = fmtNumber( now.getHours() );
+   var min  = fmtNumber( now.getMinutes() );
+   var sec  = fmtNumber( now.getSeconds() );
+   var content = year + "年" + month + "月" + date + "日" + week + "曜日" + hour + "時" + min + "分" + sec + "秒";
+   document.getElementById("RealtimeClockArea2").innerHTML = content;
+}
+setInterval('getDate()',1000);
